f615a75c5bd6467fd2718c73f9f5df4c0829822f
ININDC00076 <ININDC00076@AWS-PVDI-6.azentio.com>
2024-02-23 16:20:22 +0530

    commit from remote PC one

--- GitTutorial/src/test/java/features/TestFEATURE.feature
+++ GitTutorial/src/test/java/features/TestFEATURE.feature
@@ -1,47 +1,5 @@
-Feature: To Test the GIT functionality
-Scenario: To Test the GIT pull functionality
-Given navigate to URL
-Then verify
-Scenario: To Test the GIT Push from remote PC
-Given navigate to URL
-Then verify remote PC commit
-Scenario: To Test the GIT Push from remote PC
-Given navigate to URL
-Then verify remote PC commit Two
-Scenario: To Test the GIT Push from remote PC
-Given navigate to URL
-Then verify remote PC commit three
-Scenario: To Test the GIT Push from remote PC
-Given navigate to URL
-Then verify remote PC commit four
-Scenario: To Test the GIT Push from remote PC
-Given navigate to URL
-Then verify remote PC commit five
-Scenario: To Test the GIT Push from remote PC commit five
-Given navigate to URL
-<<<<<<< Upstream, based on origin/GitTutorial
-Then verify remote PC commit four
-=======
-Then verify remote PC commit four
->>>>>>> c933d69 Commit from remote PC
-Scenario: To Test the GIT fetch functionality
-Given navigate to URL
-Then verify fetch
-Scenario: To Test the GIT fetch functionality two
-Given navigate to URL
-Then verify fetch two
-Scenario: To Test the GIT fetch functionality three
-Given navigate to URL
-Then verify fetch three
-Scenario: To Test the GIT fetch functionality four
-Given navigate to URL
-Then verify fetch four
-Scenario: To Test the GIT fetch functionality five
-Given navigate to URL
-Then verify fetch five
-Scenario: To Test the GIT fetch functionality six
-Given navigate to URL
-Then verify fetch six
-Scenario: To Test the GIT fetch functionality six
-Given navigate to URL
-Then verify fetch seven
+Feature: To Test the git push and pull functionality
+@Test_One
+Scenario: To Test the git push and pull using eclipse git
+Given remote commit one
+
--- GitTutorial/src/test/java/stepdefenitions/gitTest.java
+++ GitTutorial/src/test/java/stepdefenitions/gitTest.java
@@ -1,78 +1,11 @@
 package stepdefenitions;
 
 import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
 
 public class gitTest {
-	@Given("^navigate to URL$")
-	public void navigate_to_url() throws Throwable {
-		System.out.println("In given step");
-	}
-
-	@Then("^verify$")
-	public void verify() throws Throwable {
-		System.out.println("In Then step");
-	}
-
-	@Then("^verify remote PC commit$")
-	public void verify_remote_PC_commit() throws Throwable {
-		System.out.println("In commit from PC step");
-	}
-
-	@Then("^verify remote PC commit Two$")
-	public void verify_remote_PC_commit_two() throws Throwable {
-		System.out.println("In commit from PC step two");
-	}
-
-	@Then("^verify remote PC commit four$")
-	public void verify_remote_PC_commit_four() throws Throwable {
-		System.out.println("In commit from PC step four");
-	}
-
-	@Then("^verify remote PC commit five$")
-	public void verify_remote_PC_commit_five() throws Throwable {
-		System.out.println("In commit from PC step five");
-	}
-
-	@Then("^verify fetch$")
-	public void verify_fetch() throws Throwable {
-		System.out.println("In Then fetch test");
-	}
-
-	@Then("^verify fetch two$")
-	public void verify_fetch_two() throws Throwable {
-		System.out.println("In Then fetch test");
-	}
-
-	@Then("^verify fetch three$")
-	public void verify_fetch_three() throws Throwable {
-		System.out.println("In Then fetch test three");
-	}
-
-	@Then("^verify fetch four$")
-	public void verify_fetch_four() throws Throwable {
-		System.out.println("In Then fetch test four");
-	}
-
-	@Then("^verify fetch five$")
-	public void verify_fetch_five() throws Throwable {
-		System.out.println("In Then fetch test five");
-	}
-
-	@Then("^verify fetch six$")
-	public void verify_fetch_six() throws Throwable {
-		System.out.println("In Then fetch test six");
-
-	}
-
-	@Then("^verify fetch seven$")
-	public void verify_fetch_seven() throws Throwable {
-		System.out.println("In Then fetch test six");
-
-	}
-
-	@Then("^verify remote PC commit three$")
-	public void verify_remote_PC_commit_three() throws Throwable {
-		System.out.println("In commit from PC step three");
-	}
+@Given("^remote commit one$")
+public void remote_commit_one()
+{
+	
+}
 }
