44818ca441a1d2c9cb18e1cf35014e92aebaaf3a
Anandh.3508 <Anandh.3508@2A34.mnw.local>
2024-02-23 16:39:06 +0530

    Local commit but remote code changed

--- GitTutorial/src/test/java/stepdefenitions/gitTest.java
+++ GitTutorial/src/test/java/stepdefenitions/gitTest.java
@@ -5,6 +5,7 @@ import io.cucumber.java.en.Given;
 public class gitTest {
 	@Given("^remote commit one$")
 	public void remote_commit_one() {
+		System.out.println("Message test remote commit one");
 
 	}
 	@Given("^local commit one$")
