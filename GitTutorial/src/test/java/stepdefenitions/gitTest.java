383cd4ff09eb56eecde091926f5ef989cf035886
ININDC00076 <ININDC00076@AWS-PVDI-6.azentio.com>
2024-02-23 15:52:28 +0530

    Commit from remote PC

--- GitTutorial/src/test/java/stepdefenitions/gitTest.java
+++ GitTutorial/src/test/java/stepdefenitions/gitTest.java
@@ -18,28 +18,39 @@ public class gitTest {
 	public void verify_remote_PC_commit() throws Throwable {
 		System.out.println("In commit from PC step");
 	}
+
 	@Then("^verify remote PC commit Two$")
 	public void verify_remote_PC_commit_two() throws Throwable {
 		System.out.println("In commit from PC step two");
 	}
+
 	@Then("^verify fetch$")
 	public void verify_fetch() throws Throwable {
 		System.out.println("In Then fetch test");
 	}
+
 	@Then("^verify fetch two$")
 	public void verify_fetch_two() throws Throwable {
 		System.out.println("In Then fetch test");
 	}
+
 	@Then("^verify fetch three$")
 	public void verify_fetch_three() throws Throwable {
 		System.out.println("In Then fetch test three");
 	}
+
 	@Then("^verify fetch four$")
 	public void verify_fetch_four() throws Throwable {
 		System.out.println("In Then fetch test four");
 	}
+
 	@Then("^verify fetch five$")
 	public void verify_fetch_five() throws Throwable {
 		System.out.println("In Then fetch test five");
+	}
+
+	@Then("^verify remote PC commit three$")
+	public void verify_remote_PC_commit_three() throws Throwable {
+		System.out.println("In commit from PC step three");
 	}
 }
